4562ae79f134dc14e1365256e3211f8a5459743c
MongieLee <le30n@outlook.com>
2021-11-24 23:22:10 +0800

    feat(Controllers):Implement the base controller for api

---
+++ src/main/java/cn/mgl/tally/controllers/BaseController.java
@@ -0,0 +1,19 @@
+package cn.mgl.tally.controllers;
+
+import cn.mgl.tally.entitys.Greeting;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class BaseController {
+    private AtomicLong counter = new AtomicLong();
+
+    @GetMapping("v1/{name}")
+    public Object say(@PathVariable("name") String name, @RequestParam(name = "age", required = false) Integer age) {
+        return new Greeting(counter.incrementAndGet(), String.format("hello %s", name));
+    }
+}
